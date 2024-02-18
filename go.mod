d411deaa02dae13e644736ab2bfd25033a4c804e
abs3nt <abs3nt@asdf.cafe>
2024-02-17 21:55:06 -0800

    gunner

---
+++ go.mod
@@ -0,0 +1,11 @@
+module git.asdf.cafe/abs3nt/gunner
+
+go 1.22.0
+
+require (
+	github.com/cristalhq/aconfig v0.18.5
+	github.com/cristalhq/aconfig/aconfigdotenv v0.17.1
+	sigs.k8s.io/yaml v1.4.0
+)
+
+require github.com/joho/godotenv v1.4.0 // indirect
---
+++ gun.go
@@ -0,0 +1,52 @@
+package gun
+
+import (
+	"os"
+	"path"
+
+	"github.com/cristalhq/aconfig"
+	"github.com/cristalhq/aconfig/aconfigdotenv"
+
+	"git.asdf.cafe/abs3nt/gunner/src/yaml"
+)
+
+func LoadApp(i any, appName string) {
+	yamlDecoder := yaml.New()
+	dotenvDecoder := aconfigdotenv.New()
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			panic(err)
+		}
+		configDir = path.Join(homeDir, ".config")
+	}
+
+	filePath := path.Join(configDir, appName)
+
+	loader := aconfig.LoaderFor(i, aconfig.Config{
+		AllowUnknownFields: true,
+		AllowUnknownEnvs:   true,
+		AllowUnknownFlags:  true,
+		SkipFlags:          true,
+		DontGenerateTags:   true,
+		MergeFiles:         true,
+		EnvPrefix:          appName,
+		FlagPrefix:         appName,
+		Files: []string{
+			path.Join(filePath, appName+".yml"),
+			path.Join(filePath, appName+".yaml"),
+			path.Join(filePath, appName+".json"),
+			path.Join(filePath, ".env"),
+		},
+		FileDecoders: map[string]aconfig.FileDecoder{
+			".yaml": yamlDecoder,
+			".yml":  yamlDecoder,
+			".json": yamlDecoder,
+			".env":  dotenvDecoder,
+		},
+	})
+	if err := loader.Load(); err != nil {
+		panic(err)
+	}
+}
